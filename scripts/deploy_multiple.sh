f26fad458f73888618b58b88853a85856fd5eb8b
Rasmus Hummelmose <rasmus@hummelmose.dk>
2019-11-12 20:21:26 +0100

    jq foreach

--- scripts/deploy_multiple.sh
+++ scripts/deploy_multiple.sh
@@ -6,5 +6,13 @@ deployment_targets="$1"
 printf "DEPLOYMENT TARGETS:\n"
 printf "%s\n" $deployment_targets
 
-# Get deployment targets
+# jq print deployment targets
 printf "%s" $deployment_targets | jq .
+
+# Print each deployment target
+deployment_targets_tsv=$(printf "%s" $deployment_targets | jq -r '.[] | [.resource-group, .cluster-name, .namespace] | @tsv')
+printf "%s" $deployment_targets_tsv | while IFS=$'\t' read -r resource_group cluster_name namespace; do
+    echo "$resource_group"
+    echo "$cluster_name"
+    echo "$namespace"
+done
