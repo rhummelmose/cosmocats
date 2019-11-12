189df4a15502e3edd83e35b3102cdaa6deedce43
Rasmus Hummelmose <rasmus@hummelmose.dk>
2019-11-12 19:08:17 +0100

    Deployment pipelines

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,9 +1,31 @@
 #!/bin/bash
 
-# Authenticate with the cluster 
-az aks get-credentials --resource-group $COSMOCATS_RESOURCE_GROUP --name $COSMOCATS_CLUSTER_NAME --admin
+# Get arguments
+resource_group=$1
+cluster_name=$2
+namespace=$3
 
-# Kubectl
+# Verify arguments
+if [ -z "$resource_group" ]; then
+    echo "Resource group name required as 1st argument.."
+    exit 1
+fi
+if [ -z "$cluster_name" ]; then
+    echo "Cluster name required as 2nd argument.."
+    exit 1
+fi
+if [ -z "$namespace" ]; then
+    echo "Namespace required as 3rd argument.."
+    exit 1
+fi
+
+# Authenticate with the cluster 
+az aks get-credentials --resource-group $resource_group --name $cluster_name --admin
+
+# Change namespace
+kubectl config set-context --current --namespace=$namespace
+
+# Deploy
 kubectl apply -f kubernetes/configmaps.yml
 kubectl apply -f kubernetes/secrets.yml
 kubectl apply -f kubernetes/deployments.yml
---
+++ scripts/deploy_multiple.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+deployment_targets="$1"
+
+# Get deployment targets
+printf "DEPLOYMENT TARGETS:\n"
+printf "%s\n" $deployment_targets
